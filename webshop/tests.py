497961e7bf5d0694db9660f7ec96cdab3494c81c
Mohammadjavad Esmaeili <mj.esmaeili73@gmail.com>
2025-01-21 16:50:47 +0100

    additional test for API

--- webshop/tests.py
+++ webshop/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Order
 
-# Create your tests here.
+class OrderTestCase(TestCase):
+    def setUp(self):
+        Order.objects.create(
+            customer_name="Max Mustermann",
+            customer_email="manx-mustermann@example.com",
+            products=[{"name": "Keyboard", "quantity": 1}],
+            total_price=50.00,
+            status="PENDING"
+        )
+
+    def test_order_creation(self):
+        order = Order.objects.get(customer_name="Max Mustermann")
+        self.assertEqual(order.status, "PENDING")
