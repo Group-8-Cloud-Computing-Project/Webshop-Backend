7f016587e50b1319ca8c437e6b2e61c709152be7
Masroor Aijaz <masrooraijaz@hotmail.com>
2025-02-28 00:36:15 +0100

    Deleted unused file.

--- api/azure_storage.py
+++
@@ -1,8 +0,0 @@
-from storages.backends.azure_storage import AzureStorage
-import os
-
-class AzureMediaStorage(AzureStorage):
-    account_name = os.getenv('AZURE_STORAGE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_STORAGE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_STORAGE_CONTAINER_NAME')
-    expiration_secs = None  # Optional: disables URL expiry
